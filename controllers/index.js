9240f2504ceb74a3e0e84c688be9c0e8206445d8
michaelmaluf <117127666+michaelmaluf@users.noreply.github.com>
2022-11-21 18:15:26 -0800

    models added mongoose conntection added

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,8 @@
 const router = require("express").Router();
-const path = require("path");
+const htmlRoutes = require("./html-routes");
+const apiRoutes = require("./api");
 
-router.get("/stats", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/stats.html"));
-});
-
-router.get("/exercise", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/exercise.html"));
-});
+router.use("/", htmlRoutes);
+router.use("/api", apiRoutes);
 
 module.exports = router;
